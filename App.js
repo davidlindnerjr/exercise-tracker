15c0863c86de8756492c0bd19e09402417711cc9
David Lindner Jr <davidlindnerjr@Davids-MacBook-Air.local>
2022-10-14 12:53:49 -0700

    Added dependencies + Set up screens and routing

--- App.js
+++ App.js
@@ -1,14 +1,102 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+// Screens
+import Homescreen from './screens/Homescreen';
+import AddExercise from './screens/AddExercise';
+import GoalScreen from './screens/GoalScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+// Navigators
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+// Constants
+import { GlobalStyles } from './constants/styles';
+
+// Components
+import IconButton from './components/UI/IconButton';
+
+const ExercisesOverview = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabs.Navigator screenOptions={({navigation}) => ({ 
+      headerStyle: {
+        backgroundColor: GlobalStyles.colors.darkBlue,
+      },
+      headerTintColor: GlobalStyles.colors.white,
+      tabBarStyle: {
+        backgroundColor: GlobalStyles.colors.darkBlue,
+      },
+      tabBarActiveTintColor: GlobalStyles.colors.white,
+      tabBarInactiveTintColor: GlobalStyles.colors.white,
+      headerRight: ({ tintColor }) => {
+        return <IconButton icon="add" size={24} color={tintColor} onPress={()=>{navigation.navigate('AddExercise')}}/>
+      }
+    })}>
+      <BottomTabs.Screen 
+        name='Homescreen' 
+        component={Homescreen} 
+        options={{
+           title: 'Home',
+           tabBarLabel: 'Home',
+           tabBarIcon: ({color, size}) => <Ionicons name='home-outline' size={size} color={color} />
+        }}
+      />
+      <BottomTabs.Screen 
+        name='GoalScreen' 
+        component={GoalScreen}
+        options={{
+          style: styles.removeBorder,
+          title: 'My Goals',
+          tabBarLabel: 'Goals',
+          tabBarIcon: ({color, size}) => <Ionicons name='checkbox-outline' size={size} color={color} />
+       }} 
+      />
+       <BottomTabs.Screen 
+        name='SettingsScreen' 
+        component={SettingsScreen}
+        options={{
+          style: styles.removeBorder,
+          title: 'User Settings',
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => <Ionicons name='settings-outline' size={size} color={color} />
+       }} 
+      />
+    </BottomTabs.Navigator>
   );
 }
+
+const App = () => {
+  return (
+    <>
+      <StatusBar style="light-content" />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {
+          backgroundColor: GlobalStyles.colors.darkBlue,
+        },
+          headerTintColor: 'white'
+        }}>
+          <Stack.Screen 
+            name='ExercisesOverview' 
+            component={ExercisesOverview} 
+            options={{ headerShown: false, style: styles.removeBorder}}
+          />
+          <Stack.Screen name='AddExercise' component={AddExercise}  options={{
+            presentation: 'modal'
+          }}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +105,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  removeBorder: {
+    backgroundColor: GlobalStyles.colors.darkBlue,
+    borderTopWidth: 0,
+  }
 });
---
+++ constants/styles.js
@@ -0,0 +1,12 @@
+export const GlobalStyles = {
+    colors: {
+        white: '#FFFFFF',
+        darkBlue: '#073b4c',
+        lightBlue: '#118ab2',
+        green: '#06d6a0',
+        yellow: '#ffd166',
+        red: 'ef476f',
+        error: '#FF0000',
+        success: '#0FFF50'
+    }
+}
